babb2b4f52c09d60e0da30a1a9ffe955537e67e8
Andrew C <acchang4@gmail.com>
2020-04-08 20:15:06 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -4,6 +4,9 @@ def input_to_index(input)
   input_to_index=index
 end
 
+def move(board, index, player="X")
+   board[index] = player
+end
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -11,10 +14,6 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def move(board, index, player="X")
-   board[index] = player
 end
 
 def update_array_at_with(array, index, value)
