b26e8332b1ff7c12f1b1934deb59116475faad06
Andrew C <acchang4@gmail.com>
2020-04-08 19:33:08 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -2,7 +2,7 @@
 
 board.to_i
 
-def display_board(space)
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
